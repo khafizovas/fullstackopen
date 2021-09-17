d243eeb86dcc14aa4302a3429aab16fd382baa21
khafizovas <justhomosapiens121@gmail.com>
2021-09-18 00:29:01 +0300

    add new Statistics Categories

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -2,12 +2,19 @@ import Category from './Category';
 import Header from './Header';
 
 const Statistics = (props) => {
+	const sum = props.good + props.neutral + props.bad;
+	const average = (props.good - props.bad) / sum || 0;
+	const positive = (props.good * 100) / sum || 0 + '%';
+
 	return (
 		<div>
 			<Header heading={'Statistics'} />
 			<Category name='Good' value={props.good} />
 			<Category name='Neutral' value={props.neutral} />
 			<Category name='Bad' value={props.bad} />
+			<Category name='All' value={sum} />
+			<Category name='Average' value={average} />
+			<Category name='Positive' value={positive} />
 		</div>
 	);
 };
