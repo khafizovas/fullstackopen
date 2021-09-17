11727039a0919abc6b3150b5258c76fdf7749aaf
khafizovas <justhomosapiens121@gmail.com>
2021-09-17 16:57:44 +0300

    create components files and implement Header component

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Header from './Header';
+
 const App = () => {
 	const course = 'Half Stack application development';
 	const part1 = 'Fundamentals of React';
@@ -11,7 +13,7 @@ const App = () => {
 
 	return (
 		<div>
-			<h1>{course}</h1>
+			<Header course={course} />
 			<p>
 				{part1} {exercises1}
 			</p>
