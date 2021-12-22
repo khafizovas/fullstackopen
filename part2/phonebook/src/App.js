d5baf70bb85d485303e2d78ed3d5558d59f63d67
khafizovas <justhomosapiens121@gmail.com>
2021-12-23 02:41:52 +0300

    Added Phonebook step 1

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,26 +1,37 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ])
-  const [newName, setNewName] = useState('')
+    const [persons, setPersons] = useState([
+        {name: 'Arto Hellas'}
+    ])
+    const [newName, setNewName] = useState('')
 
-  return (
-      <div>
-        <h2>Phonebook</h2>
-        <form>
-          <div>
-            name: <input />
-          </div>
-          <div>
-            <button type="submit">add</button>
-          </div>
-        </form>
-        <h2>Numbers</h2>
-        ...
-      </div>
-  )
+    const handleChange = (e) => {
+        setNewName(e.target.value)
+    }
+
+    const addNewPerson = (e) => {
+        e.preventDefault()
+        setPersons([...persons, {name: newName}])
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={addNewPerson}>
+                <div>
+                    name: <input value={newName} onInput={handleChange}/>
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <ul>
+                {persons.map(person => <li key={person.name}>{person.name}</li>)}
+            </ul>
+        </div>
+    )
 }
 
 export default App
