83142a8c342144a680b8e2e3e2c0f758007fa81a
khafizovas <justhomosapiens121@gmail.com>
2021-12-23 01:49:50 +0300

    Refactored step 8

--- part2/courseinfo/src/Total.js
+++ part2/courseinfo/src/Total.js
@@ -1,5 +1,7 @@
 const Total = ({exercises}) => {
-    const sum = exercises.reduce((sum, cur) => sum + cur, 0)
+    const sum = exercises.reduce((sum, cur) => {
+        return sum + cur
+    }, 0)
 
     return (
         <p><strong>Total of {sum} exercises</strong></p>
