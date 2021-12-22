cb2c03a78c6ae3ded34818bf8d01a2a13268535e
khafizovas <justhomosapiens121@gmail.com>
2021-12-23 01:44:38 +0300

    Added step 7

--- part2/courseinfo/src/Course.js
+++ part2/courseinfo/src/Course.js
@@ -1,13 +1,13 @@
 import Header from "./Header";
 import Content from "./Content";
-// import Total from "./Total";
+import Total from "./Total";
 
 const Course = ({course}) => {
     return (
         <div>
             <Header name={course.name}/>
             <Content parts={course.parts}/>
-            {/*<Total exercises={course.parts.map(part => part.exercises)}/>*/}
+            <Total exercises={course.parts.map(part => part.exercises)}/>
         </div>
     )
 }
--- part2/courseinfo/src/Total.js
+++ part2/courseinfo/src/Total.js
@@ -2,7 +2,7 @@ const Total = ({exercises}) => {
     const sum = exercises.reduce((sum, cur) => sum + cur, 0)
 
     return (
-        <p>Number of exercises {sum}</p>
+        <p><strong>Total of {sum} exercises</strong></p>
     )
 }
 
