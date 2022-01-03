b907918bfa7f77e8158543b11e0eead3e7949e15
khafizovas <justhomosapiens121@gmail.com>
2022-01-04 00:33:22 +0300

    Added Data for countries step 3

--- part2/countries/src/Country.js
+++ part2/countries/src/Country.js
@@ -1,4 +1,18 @@
+import axios from 'axios';
+
+import {useState, useEffect} from 'react';
+
 const Country = ({country}) => {
+    const [weather, setWeather] = useState(null);
+
+    useEffect(() => {
+        axios
+            .get(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${country.capital}&appid=9f3631b6a2b56abcfe98f9931f76f9bf`)
+            .then(response => {
+                setWeather(response.data);
+            });
+    }, [country]);
+
     return (<div>
         <h2>{country.name.common}</h2>
         <p>Capital: {country.capital}</p>
@@ -8,6 +22,10 @@ const Country = ({country}) => {
             .map((language, i) => <li key={i}>{language}</li>)}
         </ul>
         <img src={country.flags.png} alt={'Country flag'}/>
+        <h3>Weather in {country.capital}</h3>
+        <p><strong>Temperature:</strong> {weather?.main.temp}</p>
+        <img src={`http://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`} alt="Weather icon"/>
+        <p><strong>Wind:</strong> {`${weather?.wind.speed} mph, direction: ${weather?.wind.deg} deg`}</p>
     </div>);
 };
 
