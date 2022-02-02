686b1918342613daba6498d0ae45526fcbda4af6
khafizovas <justhomosapiens121@gmail.com>
2022-02-02 21:19:49 +0300

    Fixed app key

--- part2/countries/src/Country.js
+++ part2/countries/src/Country.js
@@ -1,32 +1,48 @@
 import axios from 'axios';
 
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
-const Country = ({country}) => {
-    const [weather, setWeather] = useState(null);
+const Country = ({ country }) => {
+	const [weather, setWeather] = useState(null);
 
-    useEffect(() => {
-        axios
-            .get(`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${country.capital}&appid=9f3631b6a2b56abcfe98f9931f76f9bf`)
-            .then(response => {
-                setWeather(response.data);
-            });
-    }, [country]);
+	useEffect(() => {
+		axios
+			.get(
+				`https://api.openweathermap.org/data/2.5/weather?units=metric&q=${country.capital}&appid=${process.env.REACT_APP_API_KEY}`
+			)
+			.then((response) => {
+				setWeather(response.data);
+			});
+	}, [country]);
 
-    return (<div>
-        <h2>{country.name.common}</h2>
-        <p>Capital: {country.capital}</p>
-        <p>Population: {country.population}</p>
-        <h3>Languages</h3>
-        <ul>{Object.values(country.languages)
-            .map((language, i) => <li key={i}>{language}</li>)}
-        </ul>
-        <img src={country.flags.png} alt={'Country flag'}/>
-        <h3>Weather in {country.capital}</h3>
-        <p><strong>Temperature:</strong> {weather?.main.temp}</p>
-        <img src={`http://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`} alt="Weather icon"/>
-        <p><strong>Wind:</strong> {`${weather?.wind.speed} mph, direction: ${weather?.wind.deg} deg`}</p>
-    </div>);
+	return (
+		<div>
+			<h2>{country.name.common}</h2>
+			<p>Capital: {country.capital}</p>
+			<p>Population: {country.population}</p>
+			<h3>Languages</h3>
+			<ul>
+				{Object.values(country.languages).map((language, i) => (
+					<li key={i}>{language}</li>
+				))}
+			</ul>
+			<img src={country.flags.png} alt={'Country flag'} />
+			<h3>Weather in {country.capital}</h3>
+			<p>
+				<strong>Temperature:</strong> {weather?.main.temp}
+			</p>
+			{weather && (
+				<img
+					src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+					alt='Weather icon'
+				/>
+			)}
+			<p>
+				<strong>Wind:</strong>{' '}
+				{`${weather?.wind.speed} mph, direction: ${weather?.wind.deg} deg`}
+			</p>
+		</div>
+	);
 };
 
 export default Country;
