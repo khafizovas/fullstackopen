00e071176ab1363cf4946f79ebc8f332846d5b3d
khafizovas <justhomosapiens121@gmail.com>
2022-02-02 23:05:45 +0300

    Added 3.5

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let persons = [
 	{
 		id: 1,
@@ -62,7 +64,28 @@ app.delete('/api/persons/:id', (request, response) => {
 	response.status(204).end();
 });
 
+app.post('/api/persons', (request, response) => {
+	const body = request.body;
+
+	console.log(body);
+
+	const person = {
+		name: body.name,
+		number: body.number,
+		date: new Date(),
+		id: generateId(),
+	};
+
+	persons = persons.concat(person);
+
+	response.json(person);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
+
+const generateId = () => {
+	return Math.floor(Math.random() * 1000000);
+};
